54b0f1b7fd31bf9ae9f596077bb730d69620162e
Stark <inventorstark7@gmail.com>
2024-05-08 00:52:59 +0100

    feat: server backend

---
+++ server/index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.get("/ping", (_req, res) => {
+  return res.json({ msg: "Ping Successful" });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.log("#".repeat(50))
+  console.log("#".repeat(50))
+  console.log("Error message: ", err?.message || err?.msg)
+  console.log("Error name: ", err?.name)
+  console.log("Error Stack:::::: ")
+  console.error(err.stack); // Log the error stack trace
+  console.log("#".repeat(50))
+  console.log("#".repeat(50))
+  res.status(500).send('Something broke!');
+});
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
